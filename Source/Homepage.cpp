2218385ac768b3177b984f52605d1e19b0d852d7
thomassross <th0m4sr0ss@gmail.com>
2015-10-18 20:07:43 -0400

    Add 'Play Random Game' to homepage

--- Source/Homepage.cpp
+++ Source/Homepage.cpp
@@ -6,6 +6,71 @@ Homepage::Homepage(QSettings* p, QWidget* parent) :
     ui(new Ui::Homepage)
 {
     ui->setupUi(this);
+    this->setObjectName("homepageUI");
+    this->setStyleSheet("QPushButton {"
+                        "font-family: SourceSansPro;"
+                        "background-color: " + p->value("Primary/DarkElement").toString() + ";"
+                        "color: " + p->value("Primary/LightText").toString() + ";"
+                        "border: none; margin: 0px; padding: 0px;}"
+                        "QPushButton:hover {"
+                        "background-color: " + p->value("Primary/InactiveSelection").toString() + ";} "
+                        "QLabel {"
+                        "font-family: SourceSansPro;"
+                        "color: " + p->value("Primary/LightText").toString() + " }"
+                        "#playRandom { font-size: 12px; }"
+                        "#randomGameLbl { font-size: 16px; }"
+                        "#newRandom { background-color: transparent; }");
+
+    QPixmap refresh(":/SystemMenu/Icons/Refresh_Inverted.png");
+    QIcon icon(refresh);
+    ui->newRandom->setIcon(icon);
+    ui->newRandom->setIconSize(QSize(18, 18));
+
+    if (!db.init())
+    {
+        QMessageBox error;
+        error.critical(0, "Error!", "An error occured while trying to load the database.");
+        exit(EXIT_FAILURE);
+    }
+
+    selectRandomGame();
+}
+
+void Homepage::on_newRandom_clicked()
+{
+    selectRandomGame();
+}
+
+void Homepage::on_playRandom_clicked()
+{
+    if (!gl.isProcessRunning())
+    {
+        Game game = db.getGameByName(curRandom);
+        if (game.arguments.trimmed() == "")
+        {
+            gl.runProcess(game.executablePath, game.gameDirectory);
+        }
+        else
+        {
+            gl.runProcessWithArgs(game.executablePath, game.gameDirectory, game.arguments);
+        }
+    }
+    else
+    {
+        QMessageBox messageBox;
+        messageBox.setText("Error: an application is already running.");
+        messageBox.exec();
+    }
+}
+
+void Homepage::selectRandomGame()
+{
+    QList<Game> games = db.getGames();
+    Game game = games[rand() % games.length()];
+
+    curRandom = game.gameName;
+    ui->randomGameLbl->setText("Random Game: " + game.gameName);
+    ui->randomGameLbl->adjustSize();
 }
 
 Homepage::~Homepage()
